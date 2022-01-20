29ddd262f01d9632fbf9c4cbe228ac1b36693a1c
yuliaelkina <52623041+yuliaelkina@users.noreply.github.com>
2022-01-20 13:48:58 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,2 +0,0 @@
-import './js/app';
-import './css/style.css';
--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('.banner__button');
-const banner = document.querySelector('.banner__text');
-button.addEventListener('click', (e) => {
-  e.preventDefault();
-  banner.classList.toggle('banner__text--display');
-});
